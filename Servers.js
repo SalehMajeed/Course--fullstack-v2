580f64bbcfe5de43491832589d03932ad97f2804
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-26 16:22:36 -0400

    node

--- Servers.js
+++ Servers.js
@@ -4,4 +4,7 @@
     3. VPS.
     4. Secure Socket Shell (SSH) -> work upon public and private key.
         private store on system and public goes to server.
+    5. A Record -> map name to ip.
+    6. CNAME -> map name to name.
+    7. dig -> use to know record of site. 
 */
---
+++ server_setup.js
@@ -0,0 +1,6 @@
+/*
+    1. add user ->
+            sudo adduser $USERNAME.
+    2. nodejs -> single threaded javascript engine.
+    
+*/
