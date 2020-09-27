8dd3be95c6cb226dd7a85c03f2fbdfc3b5db744a
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-27 09:55:55 -0400

    Bash And Security

---
+++ Bash_Basics.js
@@ -0,0 +1,6 @@
+/**
+    1. stdout -> output.
+    2. stdin -> input.
+    3. stderr -> error.
+    4. find -> to search files.
+*/
---
+++ security.js
@@ -0,0 +1,12 @@
+/*
+    1. SSH.
+    2. firewall -> monitoring traffic and allow or block them.
+    3. updates.
+    4. Two factor authentication.
+    5. VPN.
+    6. install unattend-upgrades.
+    7. nmap.
+    8. port.
+    9. iptables.
+    10. ufw.
+*/
